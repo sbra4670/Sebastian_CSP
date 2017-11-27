7fa0a399805bca1821988c4fe3f0e0084c7d8951
Sebastian <31518719+sbra4670@users.noreply.github.com>
2017-11-27 12:29:52 -0700

    added a swipe left protocol!

--- Sebastian_CSP/Controller/AlgorithmsViewController.swift
+++ Sebastian_CSP/Controller/AlgorithmsViewController.swift
@@ -1,73 +1,80 @@
-////
-////  AlgorithmsViewController.swift
-////  Sebastian_CSP
-////
-////  Created by Bravo, Sebastian on 11/8/17.
-////  Copyright © 2017 Bravo, Sebastian. All rights reserved.
-////
 //
-//import UIKit
+//  AlgorithmsViewController.swift
+//  Sebastian_CSP
 //
-//class AlgorithmsViewController: UIViewController {
+//  Created by Bravo, Sebastian on 11/8/17.
+//  Copyright © 2017 Bravo, Sebastian. All rights reserved.
 //
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        // Do any additional setup after loading the view.
-//    }
-//    
-//    private func setupAlgorithm()-> Void
-//    {
-//        var algorithmSteps : [String] = []
-//        
-//        //definition to algorithm and all steps
-//        let algorithm :String = "These are the instructions to create a pect in Java using Eclipse and Github"
-//        let stepOne :String = "First, open Eclipse, and right click on the project explorer window. Select new -> Java Project, name the project whatever you wish."
-//        let stepTwo :String = "Second, open Github, and select 'file' in the upper area of your screen. Select 'new repository'."
-//        let stepThree :String = "Lastly and most importantly, name your repository THE EXACT SAME as your Java Project in Eclipse"
-//        
-//        algorithmSteps = [stepOne, stepTwo, stepThree]
-//        
-//        let attributes = [NSAttributedStringKey.font : algorithmText.font]
-//        let fullAttributedString = NSMutableAttributedString(string: algorithm, attributes: attributesDictionary)
-//
-//        for step in algorithmSteps
-//        {
-//            let bullet :String = "🖤"
-//            let formattedStep :String = "\n\(bullet) \ (step)"
-//            let attributedStringStep: NSMutableAttributedString = NSMutableAttributedString(string: formattedStep)
-//            let paragraphStyle = createParagraphStyle()
-//
-//            attributedStringStep.addAttributes([NSAttributedStringKey.paragraphStyle : paragraphStyle], range:
-//            NSMakeRange(0,attributedStringStep.length))
-//            
-//            fullAttributedString.append(attributedStringStep)
-//        }
-//        
-//    }
-//    
-//    
-//    
-//    
-//    
-//    
-//    
-//
-//    override func didReceiveMemoryWarning() {
-//        super.didReceiveMemoryWarning()
-//        // Dispose of any resources that can be recreated.
-//    }
-//    
-//
-//    /*
-//    // MARK: - Navigation
-//
-//    // In a storyboard-based application, you will often want to do a little preparation before navigation
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        // Get the new view controller using segue.destinationViewController.
-//        // Pass the selected object to the new view controller.
-//    }
-//    */
-//
-//}
+
+import UIKit
+
+class AlgorithmsViewController: UIViewController {
+    @IBOutlet weak var algorithmText: UILabel!
+
+    
+    private func setupAlgorithm()-> Void
+    {
+        var algorithmSteps : [String] = []
+        
+        //definition to algorithm and all steps
+        let algorithm :String = "These are the instructions to create a pect in Java using Eclipse and Github"
+        let stepOne :String = "First, open Eclipse, and right click on the project explorer window. Select new -> Java Project, name the project whatever you wish."
+        let stepTwo :String = "Second, open Github, and select 'file' in the upper area of your screen. Select 'new repository'."
+        let stepThree :String = "Lastly and most importantly, name your repository THE EXACT SAME as your Java Project in Eclipse"
+        
+        algorithmSteps = [stepOne, stepTwo, stepThree]
+        
+        let attributesDictionary = [NSAttributedStringKey.font : algorithmText.font]
+        let fullAttributedString = NSMutableAttributedString(string: algorithm, attributes: attributesDictionary)
+        
+        for step in algorithmSteps
+        {
+            let bullet :String = "🖤"
+            let formattedStep :String = "\n\(bullet) \(step)"
+            let attributedStringStep: NSMutableAttributedString = NSMutableAttributedString(string: formattedStep)
+            let paragraphStyle = createParagraphStyle()
+            
+            attributedStringStep.addAttributes([NSAttributedStringKey.paragraphStyle : paragraphStyle], range:
+                NSMakeRange(0,attributedStringStep.length))
+            
+            fullAttributedString.append(attributedStringStep.length)
+        }
+        
+        algorithmText.attributedText = fullAttributedString
+    }
+    
+    private func createParagraphStyle() -> NSParagraphStyle
+    {
+        let paragraphStyle: NSMutableParagraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = .left
+        paragraphStyle.defaultTabInterval = 15
+        paragraphStyle.firstLineHeadIndent = 20
+        paragraphStyle.headIndent = 35
+        
+        return paragraphStyle
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
 
--- Sebastian_CSP/Controller/ViewController.swift
+++ Sebastian_CSP/Controller/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  AbstractionViewController.swift
 //  Sebastian_CSP
 //
 //  Created by Bravo, Sebastian on 10/26/17.
@@ -8,11 +8,67 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+public class AbstractionViewController: UIViewController, UIPageViewControllerDataSource
+{
+    //MARK: Array  of subviews
+    private (set) lazy var orderedAbstractionViews : [UIViewController] =
+    {
+        return [
+            self.newAbstractionViewController(abstractionLevel: "Block"),
+            self.newAbstractionViewController(abstractionLevel: "Java"),
+            self.newAbstractionViewController(abstractionLevel: "ByteCode"),
+            self.newAbstractionViewController(abstractionLevel: "Binary"),
+            self.newAbstractionViewController(abstractionLevel: "AndGate")
+        ]
+    }()
+    
+    //Helper method to retrieve th correct ViewController
+    private func newAbstractionViewController(abstractionLevel : String) -> UIViewController
+    {
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "\(abstractionLevel)ViewController")
+    }
+    
+    
+    override public func viewDidLoad()
+    {
         super.viewDidLoad()
+        dataSource = self
+        
+        if let firstViewController = orderedAbstractionViews.first
+        {
+            setViewControllers([firstViewController],
+            direction: .foward,
+            animated: true,
+            completion: nil)
+        }
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    //MARK: - Required Protocol methods for UIPageViewControllerDataSource
+    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore
+        viewController: UIViewController) -> UIViewController?
+    {
+        guard let viewControllerIndex = orderedAbstractionViews.index(of: viewController)
+        else
+        {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0
+        else
+        {
+         return orderedAbstractionViews.last
+        }
+        
+        guard orderedAbstractionViews.count > previousIndex
+        else
+        {
+            return nil
+        }
+        
+        return orderedAbstractionViews[previousIndex]
     }
 
     override func didReceiveMemoryWarning() {
